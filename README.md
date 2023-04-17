8eef5fa69a7ac88152b8c2bd8f5012b6ddba4d19
elizabeth189 <elizabethlopez0108@gmail.com>
2023-04-17 14:44:53 -0400

    added dist folder

--- README.md
+++ README.md
@@ -2,27 +2,30 @@
 
 ## Description
 
-I wanted to take what I have learned about PWA's and create this PWA app for users to use to create notes of their liking. 
-
+I wanted to take what I have learned about PWA's and create this PWA app for users to use to create notes of their liking.
 
 ## Installation
 
+npm i;
+npm start
 
 ## Usage
 
+The test [website](https:/) allows you to view the test README used for submission ![Web1](./assets/images/Web1.PNG)
 
-The test [website](https://github) allows you to view the test README used for submission ![Web1](./assets/images/Web1.PNG)
-
-
-You can look through the files to check out how I created this README generator! 
-
+You can look through the files to check out my notes PWA app!
 
 ## License
+
 Please refer to the LICENSE in the repo.
 
 ## Credits
-Class assignments and lessons related to this module were used when building this project, along with outside resources such as: 
+
+Class assignments and lessons related to this module were used when building this project, along with outside resources such as:
 -W3Schools
 -Mozilla
 -ChatGPT (great at explaining errors!)
 
+## Questions
+
+Please feel free to reach out with any questions by email or connect through [LinkedIn](https://www.linkedin.com/in/elizabeth-lopez-lopez-345b3b14a/)
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,13 +15,20 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 //strater code: export const putDb = async (content) => console.error('putDb not implemented');
 export const putDb = async (content) => {
-  const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const result = await store.put({ iid: Date.now(), todo: content });
-  await tx.done;
+  // const db = await initdb();
+  // const tx = db.transaction('jate', 'readwrite');
+  // const store = tx.objectStore('jate');
+  // const result = await store.put({ iid: Date.now(), todo: content });
+  // await tx.done;
 
-  return result;
+  // return result;
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -33,7 +40,7 @@ export const getDb = async () => {
   const result = await store.getAll();
   console.log('result.value', result);
   await tx.done;
-  
+
   return result > 0 ? result[0] : null;
 };
 
